e71aa0901d2419e22b720d6753fbf3641393c24d
MohammadArshad07 <arshadshaik55127@gmail.com>
2026-06-02 16:44:27 +0530

    fix production auth flow and env-based API/JWT config

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,26 +1,52 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("postgres://"):
+# LOAD .env FILE
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+
+if not DATABASE_URL:
+
+    raise ValueError(
+        "DATABASE_URL not found"
+    )
+
+
+# FIX RAILWAY URL
+
+if DATABASE_URL.startswith(
+    "postgres://"
+):
+
     DATABASE_URL = DATABASE_URL.replace(
         "postgres://",
         "postgresql+psycopg2://",
         1
     )
 
+
 engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
+    DATABASE_URL
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
